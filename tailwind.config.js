03bdfa0128d518df09f49f9186b3fcf3e29a3a81
unknown <Dimples>
2023-12-21 23:45:32 +0800

    fix tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: "#137300",
+        secondaryColor: "#FFD351",
+        tertiaryColor: "#FAFAFA", 
+      },
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
