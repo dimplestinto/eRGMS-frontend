86a415e39ae741dd95dfed25edaadd2fef4d8577
unknown <Dimples>
2023-12-22 23:50:13 +0800

    added new logo

--- src/components/sidebar/SidebarCustomer.jsx
+++ src/components/sidebar/SidebarCustomer.jsx
@@ -5,46 +5,47 @@ import { Link, useNavigate } from "react-router-dom";
 import { IoIosLogOut } from "react-icons/io";
 
 export const SidebarCustomer = () => {
-  
   const apiURL = "https://9488-64-226-63-220.ngrok-free.app";
-  
+
   const navigator = useNavigate();
 
   const token = localStorage.getItem("token");
-  
+
   console.log(token);
 
   const handleLogoutCustomer = async (e) => {
     e.preventDefault();
 
     try {
-      let response = await fetch(apiURL + "/eRGMS/public/api/logout-customer",
-      {
+      let response = await fetch(apiURL + "/eRGMS/public/api/logout-customer", {
         method: "DELETE",
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
-      
+
       if (response.ok) {
         setTimeout(() => {
           navigator("/");
           localStorage.clear();
-        return "Account Logout";
+          return "Account Logout";
         }, 1200);
       }
     } catch (error) {
       console.log(error);
     }
-
-  }
+  };
 
   return (
     <>
       <div className="fixed flex flex-col py-4 px-10 bg-white w-60 h-screen items-center shadow-lg justify-between">
         <div>
           <div className="w-28">
-            <img src="/public/images/Logo.png" alt="logo" className="" />
+            <img
+              src="/public/static/images/eRgmsLogo.png"
+              alt="logo"
+              className="h-16"
+            />
           </div>
           <div className="flex flex-col w-full gap-3 pt-5">
             <div className="flex gap-2 px-4 py-2 bg-primaryColor text-white font-bold rounded-lg items-center justify-start ">
@@ -66,12 +67,13 @@ export const SidebarCustomer = () => {
           </div>
         </div>
         <div className="flex flex-col w-full py-5">
-            <div 
-              onClick={handleLogoutCustomer}
-              className="flex gap-2 px-4 py-2 text-primaryColor font-bold rounded-lg items-center justify-start ">
-              <IoIosLogOut />
-              Sign Out
-            </div>
+          <div
+            onClick={handleLogoutCustomer}
+            className="flex gap-2 px-4 py-2 text-primaryColor font-bold rounded-lg items-center justify-start cursor-pointer "
+          >
+            <IoIosLogOut />
+            Sign Out
+          </div>
         </div>
       </div>
     </>
